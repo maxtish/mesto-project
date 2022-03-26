be1715bfdde18115ca3e0e95af0dc9385ea333fc
rd-max <rd-max@ya.ru>
2022-03-26 14:04:18 +0300

    add cards

--- script.js
+++ script.js
@@ -13,13 +13,9 @@ function popupClose(){
 profileEditButton.addEventListener("click", popupOpen);
 popupCloseButton.addEventListener("click", popupClose);
 
-// Находим форму в DOM
-const formElement = popup.querySelector('#popup-form'); // Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-const nameInput = popup.querySelector('#name-input'); // Воспользуйтесь инструментом .querySelector()
+const formElement = popup.querySelector('#popup-form'); // Находим форму в DOM // Воспользуйтесь методом querySelector()
+const nameInput = popup.querySelector('#name-input'); // Находим поля формы в DOM // Воспользуйтесь инструментом .querySelector()
 const jobInput =  popup.querySelector('#job-input');// Воспользуйтесь инструментом .querySelector()
-console.log(nameInput.value);
-
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
  function formSubmitHandler (evt) {
@@ -39,3 +35,54 @@ formElement.addEventListener('submit', formSubmitHandler);
 
 
 
+//При загрузке на странице должно быть 6 карточек, которые добавит JavaScript.
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];  
+const elementList = document.querySelector('#elements__list');
+  initialCards.forEach(function (item){
+  elementList.insertAdjacentHTML('beforeend', `
+    <li class="elements__element">
+        <img class="elements__image" id="elements__image" src="${item.link}" alt="Карачаевск">
+        <h2 class="elements__title" id="elements__title">${item.name}</h2>
+        <button class="elements__like" type="button" aria-label="Нравится"></button>
+    </li>
+    `);
+  })
+  
+// Функция добавления карточки
+  function addCard() {
+    let cardImg = document.querySelector('#elements__image');
+    let cardTitle = document.querySelector('#elements__title');
+    elementList.insertAdjacentHTML('beforeend', `
+    <li class="elements__element">
+        <img class="elements__image" id="elements__image" src="${initialCards[0].link}" alt="Карачаевск">
+        <h2 class="elements__title" id="elements__title">${initialCards[0].name}</h2>
+        <button class="elements__like" type="button" aria-label="Нравится"></button>
+    </li>
+    `);    
+  }
+  
